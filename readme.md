c3799d6fb49ab482ff6ebeff8b6fd6e027f1ba1b
Mark MacKay <mark@duopixel.com>
2011-06-08 13:40:47 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Extremes - a jQuery plug-in to put lists around a circle
+
+* Author: Mark MacKay [http://duopixel.ca/](http://duopixel.ca)
+* License: GPLv2/MIT
+* Meaning: Use anywhere you'd like. Credits appreciated, not required.
+
+## 1. Why?
+
+When ordering lists you will often find that topics come to a full circle; the first item is very similar to the last one. In these cases a better visual representation would be ordering them around a circle, instead top to bottom.
+
+I'm aware this is extremely limited in scope, but have you ever tried doing it in a drawing program? I'd rather stick my head in the oven and go full throttle on the gas.
+
+## 2. How?
+
+	<script src="jquery.js"></script>
+	<script src="extremes.js"></script>
+	
+**Just give me the defaults**
+
+	$('.selector').extremes();
+
+**Let me configure to my liking**
+	$('.selector').extremes({
+		diameter: 200,
+		rotation: 90 //change the initial rotation of everything
+	});
+	
+## 3. Browser support
+
+Should work in modern browsers (Firefox, Safari, Chrome, Opera, Internet Explorer 9).
+
+## 4. History
+
+Version 0.1 - Initial release
+
---
+++ src/extremes.js
@@ -0,0 +1,83 @@
+$.fn.extremes = function(options) {
+    var defaults = {
+        diameter: false,
+        background: false
+    };
+    var options = jQuery.extend(defaults, options);
+    return this.each(function() {
+      var diagram = $(this);
+       diagram.css({
+         position: "relative"
+       });
+       if (defaults.rotate) {
+         diagram.css({
+           '-webkit-transform': "rotate(" + options.rotate + "deg)",
+           '-moz-transform': "rotate(" + options.rotate + "deg)",
+           '-ms-transform': "rotate(" + options.rotate + "deg)",
+           '-o-transform': "rotate(" + options.rotate + "deg)",
+           'transform': "rotate(" + options.rotate + "deg)",
+           });
+       }
+       var elements = $(this).children();
+       var length = elements.length;
+       $(this).css({
+         "display" : "block",
+         "position:" : "relative"
+       });
+       var maxWidth = 0;
+       elements.css("float", "left");
+       elements.each(function(i){
+         var width = $(this).width();
+         if (width > maxWidth) {
+           maxWidth = width;
+         }
+         
+         var element = $(this);
+         var height = $(this).height();
+         var degrees =  i/length * 360 - 90;
+         var position = "0 50%";
+         var indent = (defaults.diameter) ? "0 0 0 " + defaults.diameter + "px" : "0 0 0 " + height * (length /6) + "px";
+         element.wrap("<div class='extremes-div'><div class='extremes-inner' /></div>");
+         $(this).parent().parent().css({
+           "-webkit-transform-origin": position,
+           "-moz-transform-origin":    position,
+           "-o-transform-origin":      position,
+           "-ms-transform-origin":     position,
+           "transform-origin":         position,
+           "-webkit-transform":        "rotate(" + degrees + "deg)",
+           "-moz-transform":           "rotate(" + degrees + "deg)",
+           "-o-transform":             "rotate(" + degrees + "deg)",
+           "-ms-transform":            "rotate(" + degrees + "deg)",
+           "transform":                "rotate(" + degrees + "deg)",
+           "padding":                   indent, 
+           "position":                 "absolute",
+           "top":                      "50%",
+           "left":                     "50%",
+           "white-space" :             "nowrap"
+         });
+          if (degrees > 90) {
+            $(this).parent().css({
+              "-webkit-transform-origin":  "50% 50%",
+               "-moz-transform-origin":    "50% 50%",
+               "-o-transform-origin":      "50% 50%",
+               "-ms-transform-origin":     "50% 50%",
+               "transform-origin":         "50% 50%",
+               "-webkit-transform":        "rotate(180deg)",
+               "-moz-transform":           "rotate(" + degrees + "deg)",
+               "-o-transform":             "rotate(" + degrees + "deg)",
+               "-ms-transform":            "rotate(" + degrees + "deg)",
+               "transform":                "rotate(" + degrees + "deg)",
+               "position":                 "absolute"
+            });
+            $(this).parent().parent().css({
+              width: $(this).parent().width(),
+              height: $(this).parent().height(), 
+              })
+          }
+       });
+       diagram.css({
+         "width" : maxWidth * 3 + defaults.diameter || 100,
+         "height" : maxWidth * 3 + defaults.diameter || 100
+       });
+  });
+};
